629d236b543b7d5d13386c14d60326cc932fbd2a
Damilola Alao <oluwadamilola.alao@gmail.com>
2022-01-17 19:38:06 +0100

    first commit

---
+++ contracts/Token.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string public name = "Dave Coin";
+    string public symbol = "dave";
+    uint256 public totalSupply = 1000000;
+    mapping(address => uint256) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
